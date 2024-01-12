bd3bbd148433c301b770a50eead8619807455ad1
unknown <idyllistoic@gmail.com>
2024-01-12 17:45:40 +0100

    START

---
+++ project1/src/plants/cournot.py
@@ -0,0 +1,5 @@
+from plants.plant import Plant
+
+class Cournot(Plant):
+    def __init__(self):
+        pass
---
+++ project1/src/plants/plant3.py
@@ -0,0 +1,5 @@
+from plants.plant import Plant
+
+class Plant3(Plant):
+    def __init__(self):
+        pass

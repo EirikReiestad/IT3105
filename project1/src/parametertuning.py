f213fa641fa6f5babb9176e66ea9842c644e0357
unknown <idyllistoic@gmail.com>
2024-01-15 14:44:24 +0100

    FIX

--- project1/src/parametertuning.py
+++ project1/src/parametertuning.py
@@ -15,7 +15,10 @@ def tune(system_params, tuning_params):
         for j in range(
             tuning_params["hidden_layers"][2],
             tuning_params["hidden_layers"][3] + 1)]
-    activation_func = tuning_params["activation_func"]
+    activation_func = list(list(np.linspace(tuning_params["activation_func"][0],
+                              tuning_params["activation_func"][1],
+                              tuning_params["activation_func"][2],
+                              dtype=int)) for i in range(len(hidden_layers)))
     epochs = list(np.linspace(tuning_params["epochs"][0],
                               tuning_params["epochs"][1],
                               tuning_params["epochs"][2],
@@ -58,10 +61,10 @@ def tune(system_params, tuning_params):
 
         system = System(parameters, visualize=False)
         mse = system.run()
-        print("MSE: ", mse[-1])
+        print("MSE: ", sum(mse))
 
-        if mse[-1] < lowest_mse:
-            lowest_mse = mse[-1]
+        if sum(mse) < lowest_mse:
+            lowest_mse = sum(mse)
             best_params = params
 
     print("\n===== Best Parameters: =====")
@@ -74,10 +77,10 @@ def tune(system_params, tuning_params):
     return best_params
 
 
-def read_configuration(config_path) -> dict:
+def read_configuration(config_path, section="DEFAULT") -> dict:
     config = configparser.RawConfigParser()
     config.read(config_path)
-    parameters = dict(config.items("DEFAULT"))
+    parameters = dict(config.items(section))
 
     for k, v in parameters.items():
         try:
@@ -95,7 +98,7 @@ def read_configuration(config_path) -> dict:
 
 if __name__ == "__main__":
     from src.system import System
-    parameters_conf = read_configuration("parameters.conf")
+    parameters_conf = read_configuration("parameters.conf", "PLANT3_NN")
     parametertuning_conf = read_configuration("parametertuning.conf")
 
     parameters = tune(parameters_conf, parametertuning_conf)

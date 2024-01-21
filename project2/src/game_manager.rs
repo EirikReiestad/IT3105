9195d4e4b1bd3462111c292817fed9a4748a57f2
EirikReiestad <eirik@reiestad.no>
2024-01-21 18:51:15 +0100

    game improvements

--- project2/src/game_manager.rs
+++ project2/src/game_manager.rs
@@ -1,14 +1,22 @@
 use crate::card::{Card, Suit};
 use rand::seq::SliceRandom;
-use rand::thread_rng;
+use rand::{thread_rng, Rng};
+use std::collections::HashMap;
+use std::io;
 
 pub struct Player {
     cards: (Card, Card),
+    value: u32,
 }
 
 impl Player {
     pub fn new(cards: (Card, Card)) -> Player {
-        Player { cards }
+        let value = 100; // TODO: Implement hand value
+        Player { cards, value }
+    }
+
+    pub fn bet(&mut self, amount: u32) {
+        self.value -= amount;
     }
 }
 
@@ -18,12 +26,42 @@ impl std::fmt::Display for Player {
     }
 }
 
+#[derive(PartialEq)]
+enum GameStage {
+    PreFlop,
+    Flop,
+    Turn,
+    River,
+}
+
+impl std::fmt::Display for GameStage {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let stage = match self {
+            GameStage::PreFlop => "PreFlop",
+            GameStage::Flop => "Flop",
+            GameStage::Turn => "Turn",
+            GameStage::River => "River",
+        };
+        write!(f, "{}", stage)
+    }
+}
+
+struct Action {
+    fold: bool,
+    check: bool,
+    call: bool,
+    raise: bool,
+    all_in: bool,
+}
+
 pub struct GameManager {
     players: Vec<Player>,
-    stack: Vec<Card>,
     flop: (Card, Card, Card),
     turn: Card,
     river: Card,
+    game_stage: GameStage,
+    dealer: usize,
+    minimum_bet: u32,
 }
 
 impl GameManager {
@@ -55,19 +93,27 @@ impl GameManager {
         let turn = stack.pop().expect(empty_stack_error);
         let river = stack.pop().expect(empty_stack_error);
 
+        // Determine dealer
+        let dealer = rng.gen_range(0..num_players as usize);
+
+        // Determine minimum bet
+        let minimum_bet = 10;
+
         GameManager {
             players,
-            stack,
             flop,
             turn,
             river,
+            game_stage: GameStage::PreFlop,
+            dealer,
+            minimum_bet,
         }
     }
 
     fn generate_stack() -> Vec<Card> {
         let mut stack = Vec::with_capacity(52);
-        for (i, suit) in (0..4).enumerate() {
-            let suit = match suit {
+        for i in 0..4 {
+            let suit = match i {
                 0 => Suit::CLUBS,
                 1 => Suit::DIAMONDS,
                 2 => Suit::HEARTS,
@@ -82,8 +128,85 @@ impl GameManager {
         stack
     }
 
-    pub fn run(&self) {
-        println!("Game is running");
+    pub fn run(&mut self) {
+        match self.game_stage {
+            GameStage::PreFlop => self.run_preflop(),
+            GameStage::Flop => self.run_flop(),
+            GameStage::Turn => self.run_turn(),
+            GameStage::River => self.run_river(),
+        }
+    }
+
+    fn run_preflop(&mut self) {
+        let mut pot = 0;
+        let mut folders: HashMap<usize, bool> = HashMap::new();
+        let mut highest_bet = 0;
+        let mut bets: HashMap<usize, u32> = HashMap::new();
+
+        for i in 0..self.players.len() {
+            let turn = (self.dealer + i + 1) % self.players.len();
+            if folders.contains_key(&turn) {
+                continue;
+            }
+            println!(" ");
+            println!("--- Player {}'s turn ---", turn);
+
+            let mut user_input = String::new();
+            if i == 0 {
+                println!(
+                    "Player {} is the small blind and must bet {}",
+                    turn, self.minimum_bet
+                );
+                pot += self.minimum_bet;
+                self.players[turn].bet(self.minimum_bet);
+            } else if i == 1 {
+                // Big blind
+                println!(
+                    "Player {} is the big blind and must bet {}",
+                    turn,
+                    self.minimum_bet * 2
+                );
+                pot += self.minimum_bet;
+                self.players[turn].bet(self.minimum_bet);
+            } else {
+                let player_bet = bets.get(&turn).unwrap_or(&0);
+                let options = if player_bet < &highest_bet {
+                    "Fold(0) Call(1) Raise(2) All In(3)"
+                } else {
+                    "Fold(0) Check(1) Raise(2) All In(3)"
+                };
+                println!("Options: {}", options);
+                loop {
+                    match io::stdin().read_line(&mut user_input) {
+                        Ok(_) => {
+                            let trimmed_input = user_input.trim();
+
+                            match trimmed_input {
+                                "0" => {}
+                                    folders.insert(turn, true);
+                                    println!("Player {} folds", turn);
+                                    break;
+                                }
+                        _  => {}
+                            }
+                        }
+                        Err(error) => println!("error: {}", error),
+                    }
+                }
+            }
+        }
+    }
+
+    fn run_flop(&self) {
+        println!("Flop");
+    }
+
+    fn run_turn(&self) {
+        println!("Turn");
+    }
+
+    fn run_river(&self) {
+        println!("River");
     }
 }
 
@@ -91,14 +214,35 @@ impl std::fmt::Display for GameManager {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         writeln!(
             f,
-            "=============================== GAME MANAGER ==============================="
+            "
+============================================================================
+=============================== GAME MANAGER ===============================
+============================================================================
+        "
         )?;
-        writeln!(f, "Flop: {} {} {}", self.flop.0, self.flop.1, self.flop.2)?;
-        writeln!(f, "Turn: {}", self.turn)?;
-        writeln!(f, "River: {}", self.river)?;
-        writeln!(f, "(1) Players:")?;
+        writeln!(f, "===== {} =====", self.game_stage)?;
+
+        if [GameStage::Flop, GameStage::Turn, GameStage::River].contains(&self.game_stage) {
+            writeln!(f, "Flop: {} {} {}", self.flop.0, self.flop.1, self.flop.2)?;
+        }
+        if [GameStage::Turn, GameStage::River].contains(&self.game_stage) {
+            writeln!(f, "Turn: {}", self.turn)?;
+        }
+        if [GameStage::River].contains(&self.game_stage) {
+            writeln!(f, "River: {}", self.river)?;
+        }
+        writeln!(f, " ")?;
+        writeln!(f, "===== PLAYERS =====")?;
         for (i, player) in self.players.iter().enumerate() {
-            write!(f, "Player {}: {}", i, player)?;
+            if i == self.dealer {
+                writeln!(f, "Player {} (Dealer): {}", i, player)?;
+            } else if i == (self.dealer + 1) % self.players.len() {
+                writeln!(f, "Player {} (Small Blind): {}", i, player)?;
+            } else if i == (self.dealer + 2) % self.players.len() {
+                writeln!(f, "Player {} (Big Blind): {}", i, player)?;
+            } else {
+                writeln!(f, "Player {}: {}", i, player)?;
+            }
         }
         Ok(())
     }
@@ -119,9 +263,5 @@ mod tests {
         let num_players = 2;
         let game_manager = GameManager::new(num_players);
         assert_eq!(game_manager.players.len(), num_players as usize);
-        assert_eq!(
-            game_manager.stack.len(),
-            52 - 2 * num_players as usize - 3 - 1 - 1 // 52 cards - 2 cards per player - flop - turn - river
-        );
     }
 }
--- project2/src/main.rs
+++ project2/src/main.rs
@@ -5,8 +5,8 @@ mod game_manager;
 use game_manager::GameManager;
 
 fn main() {
-    let num_players = 2;
-    let game_manager = GameManager::new(num_players);
+    let num_players = 6;
+    let mut game_manager = GameManager::new(num_players);
 
     game_manager.run();
     println!("{}", game_manager);

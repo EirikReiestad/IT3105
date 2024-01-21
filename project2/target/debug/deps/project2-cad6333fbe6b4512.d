83b5e1ac3f4031c6b24b9afcd6ee14c31d9ed3cc
EirikReiestad <eirik@reiestad.no>
2024-01-21 17:44:24 +0100

    fixed test

--- project2/target/debug/deps/project2-cad6333fbe6b4512.d
+++ project2/target/debug/deps/project2-cad6333fbe6b4512.d
@@ -1,12 +1,11 @@
-/home/eirik/Documents/university/IT3105/project2/target/debug/deps/project2-cad6333fbe6b4512.rmeta: src/main.rs src/card.rs src/game_manager.rs src/hands.rs Cargo.toml
+/home/eirik/Documents/university/IT3105/project2/target/debug/deps/project2-cad6333fbe6b4512.rmeta: src/main.rs src/card.rs src/game_manager.rs Cargo.toml
 
-/home/eirik/Documents/university/IT3105/project2/target/debug/deps/project2-cad6333fbe6b4512.d: src/main.rs src/card.rs src/game_manager.rs src/hands.rs Cargo.toml
+/home/eirik/Documents/university/IT3105/project2/target/debug/deps/project2-cad6333fbe6b4512.d: src/main.rs src/card.rs src/game_manager.rs Cargo.toml
 
 src/main.rs:
 src/card.rs:
 src/game_manager.rs:
-src/hands.rs:
 Cargo.toml:
 
-# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
 # env-dep:CLIPPY_CONF_DIR

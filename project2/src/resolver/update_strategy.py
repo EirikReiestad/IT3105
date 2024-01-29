2ac3a7e13cd4d96dd478172fa172f9896ded81d5
unknown <idyllistoic@gmail.com>
2024-01-29 23:28:23 +0100

    FIX

--- project2/src/resolver/update_strategy.py
+++ project2/src/resolver/update_strategy.py
@@ -1,17 +1,25 @@
+import numpy as np
 from src.poker_oracle.oracle import Oracle
+from src.game_state.states import PlayerState
 
 def updateStrategy(node):
     s = node.state
     for c in node.successors:
         updateStrategy(c)
-    if playerState(S):
-        P = Player(S)
-        for h in Oracle.generate_all_hole_pairs():
-            for a in Actions(S):
-                R_s = R_s + [v_p(S(a))[h] - v_p(S)[h]]
-                R_s_plus = max(0, R_s(h,a))
-        for h in Oracle.generate_all_hole_pairs():
-            for a in Actions(S):
-                sigma_s(h, a) = R_s_plus(h,a)/sum([R_s_plus(h, a) for a in Actions(S)])
+    if type(s) == PlayerState:
+        P = s
+        all_hole_pairs = Oracle.generate_all_hole_pairs()
+        sigma_s = np.zeros((len(all_hole_pairs), len(s.actions)))
+        R_s = np.zeros((len(all_hole_pairs), len(s.actions)))
+        R_s_plus = np.zeros((len(all_hole_pairs), len(s.actions)))
+
+        for h in range(len(all_hole_pairs)):
+            # MAYBE NEED TO ADD A SET OF ACTIONS IN PLAYER STATE
+            for a in s.actions:
+                R_s[h][a] = R_s[h][a] + [v_p(S(a))[h] - v_p(S)[h]]
+                R_s_plus[h][a] = max(0, R_s)
+        for h in range(len(all_hole_pairs)):
+            for a in s.actions:
+                sigma_s[h][a] = R_s_plus[h][a]/sum([R_s_plus[h, a_p] for a_p in s.actions])
     
     return sigma_s

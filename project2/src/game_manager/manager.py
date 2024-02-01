37a51fccf807e736468f24079535d503635bc04c
Eirik Reiestad <47319221+EirikReiestad@users.noreply.github.com>
2024-02-01 16:15:18 +0100

    Fix game manager (#12)
    * tried to make it so it stores more data and is more scalable
    
    * added state manager

--- project2/src/game_manager/manager.py
+++ project2/src/game_manager/manager.py
@@ -1,85 +1,24 @@
-from src.poker_oracle.deck import Deck, Card
-from src.game_state.states import GameState, PlayerState, BoardState
-from .players import Players, Player
 from typing import List, Tuple, Optional
-from dataclasses import dataclass
+import random
+from src.poker_oracle.deck import Deck
+from src.game_state.player_state import PublicPlayerState
+from src.game_state.board_state import PublicBoardState, PrivateBoardState
+from src.game_state.game_state import PublicGameState
+from .players import Players
 from .game_stage import GameStage
 from .game_action import Action
-import random
-
-
-@dataclass
-class Board:
-    def __init__(
-        self,
-        flop: Tuple[Card, Card, Card],
-        turn: Card,
-        river: Card,
-        pot: int,
-        highest_bet: int,
-        dealer: int,
-    ):
-        if not isinstance(flop, tuple):
-            raise TypeError("flop must be a tuple")
-        if len(flop) != 3:
-            raise ValueError("flop must be a tuple of length 3")
-        if not isinstance(turn, Card):
-            raise TypeError("turn must be a Card")
-        if not isinstance(river, Card):
-            raise TypeError("river must be a Card")
-        self.flop = flop  # A tuple of three Card instances
-        self.turn = turn  # A Card instance
-        self.river = river  # A Card instance
-        self.pot = pot  # An integer
-        self.highest_bet = highest_bet  # An integer
-        self.dealer = dealer  # An integer
 
 
 class GameManager:
-    def __init__(self, num_players: int):
-        # Generate a stack of cards and shuffle them
-        deck = Deck()
-        deck.reset_stack()
-
-        # Error if there are not enough cards for the game
-
-        # Deal cards to players
-        players = []
-
-        for _ in range(num_players):
-            first_card = deck.pop()
-            second_card = deck.pop()
-
-            if not isinstance(first_card, Card):
-                raise TypeError(
-                    "first_card must be a Card, not {}".format(type(first_card))
-                )
-            if not isinstance(second_card, Card):
-                raise TypeError(
-                    "second_card must be a Card, not {}".format(type(second_card))
-                )
-
-            players.append(Player((first_card, second_card)))
-
-        # Deal flop, turn, and river
-        flop = (deck.pop(), deck.pop(), deck.pop())
-        turn = deck.pop()
-        river = deck.pop()
-
-        for card in flop:
-            if not isinstance(card, Card):
-                raise TypeError("card must be a Card")
-        if not isinstance(turn, Card):
-            raise TypeError("turn must be a Card")
-        if not isinstance(river, Card):
-            raise TypeError("river must be a Card")
-
+    def __init__(self, num_players: int, deck: Deck):
         # Determine dealer
         dealer: int = random.randint(0, num_players - 1)
+        self.board: PrivateBoardState = PrivateBoardState(0, 0)
+        deck = self.board.reset_round(deck, dealer)
+        self.players: Players = Players(num_players)
+        deck = self.players.reset_round(deck)
 
-        self.players: Players = Players(players)
         self.game_stage: GameStage = GameStage.PreFlop
-        self.board: Board = Board(flop, turn, river, 0, 0, dealer)
         self.buy_in: int = 10
 
     def get_action(self) -> Tuple[Action, Optional[int]]:
@@ -107,39 +46,20 @@ class GameManager:
         return get_input()
 
     # Handles generating the game state
-    def get_current_state(self) -> GameState:
-        player_states: List[PlayerState] = self.get_player_states()
-        board_state: BoardState = self.get_board_state()
+    def get_current_state(self) -> PublicGameState:
+        player_states: List[PublicPlayerState] = self.get_player_states()
+        board_state: PublicBoardState = self.get_board_state()
         game_stage: GameStage = self.game_stage
 
-        return GameState(player_states, board_state, game_stage)
+        return PublicGameState(player_states, board_state, game_stage)
 
-    def get_player_states(self) -> List[PlayerState]:
+    def get_player_states(self) -> List[PublicPlayerState]:
         player_states = []
         for player in self.players:
-            player_state = PlayerState(player.chips, player.folded, player.bet)
+            player_state = PublicPlayerState(
+                player.chips, player.folded, player.bet)
             player_states.append(player_state)
         return player_states
-
-    def get_board_state(self):
-        if self.game_stage == GameStage.PreFlop:
-            cards = []
-        elif self.game_stage == GameStage.Flop:
-            flop = self.board.flop
-            cards = [flop[0], flop[1], flop[2]]
-        elif self.game_stage == GameStage.Turn:
-            flop = self.board.flop
-            turn = self.board.turn
-            cards = [flop[0], flop[1], flop[2], turn]
-        elif self.game_stage == GameStage.River:
-            flop = self.board.flop
-            turn = self.board.turn
-            river = self.board.river
-            cards = [flop[0], flop[1], flop[2], turn, river]
-
-        return BoardState(
-            cards, self.board.pot, self.board.highest_bet, self.board.dealer
-        )
 
     # Implements the rules
 
@@ -149,20 +69,23 @@ class GameManager:
     # The bet is added to the pot
     # The bet is added to the player_bets HashMap
     # The highest_bet is updated
-    def make_bet(self, player, bet):
-        if not self.players.place_bet(player, bet):
-            print(
-                "Not enough money. Folded."
-            )  # TODO: Do not need to fold. Can go all-in or check if possible
-            self.players.fold(player)
+    def make_bet(self, player: int, action: Action, bet: int):
+        if not self.players.action(player, action, bet):
+            self.players.action(player, Action.Fold, 0)
         else:
             player_bet = self.players.get_bet(player)
             self.board.pot += bet
-            print(f"Player bet: {player_bet}, bet {bet}")
             self.board.highest_bet = player_bet
+            print(f"Player bet: {player_bet}, bet {bet}")
 
-    def rotate_dealer(self):
-        self.board.dealer = (self.board.dealer + 1) % len(self.players)
+    def reset_round(self, deck: Deck):
+        """
+        Resets the round data
+        """
+        self.game_stage = GameStage.PreFlop
+        deck = self.players.reset_round(deck)
+        dealer = self.get_new_dealer(self.board.dealer)
+        deck = self.board.reset_round(deck, dealer)
 
     # Implements the game logic
 
@@ -173,81 +96,122 @@ class GameManager:
     # - Flop
     # - Turn
     # - River
-    def run(self):
-        # TODO: Add rotation of dealer
+
+    def run_game(self):
+        """
+        Runs the game, multiple rounds
+        """
+        print("Running game")
+        # TODO: Remove the generation of deck, it should ideally be passed in
         while True:
+            deck = Deck()
+            deck.reset_stack()
+            self.run_round()
+            self.reset_round(deck)
+
+    def run_round(self):
+        """
+        Runs a single round, meaning from the pre-flop to the river
+        """
+        while True:
+            print(self)
             if self.game_stage == GameStage.PreFlop:
                 winner = self.run_game_stage()
-                if winner is not None:
-                    print(f"Player {winner} won {self.board.pot} units!")
-                    break
+                if self.round_winner(winner):
+                    return
                 self.game_stage = GameStage.Flop
             elif self.game_stage == GameStage.Flop:
-                self.run_game_stage()
+                winner = self.run_game_stage()
+                if self.round_winner(winner):
+                    return
                 self.game_stage = GameStage.Turn
             elif self.game_stage == GameStage.Turn:
-                self.run_game_stage()
+                winner = self.run_game_stage()
+                if self.round_winner(winner):
+                    return
                 self.game_stage = GameStage.River
             elif self.game_stage == GameStage.River:
-                self.run_game_stage()
+                if self.round_winner(winner):
+                    return
+                self.round_winner(winner)
                 break
 
+    def round_winner(self, winner: int) -> bool:
+        """
+        Handles the winner of the round
+        """
+        if winner is None:
+            return False
+        self.players.winner(winner, self.board.pot)
+        print(f"Player {winner} won {self.board.pot} units!")
+        return True
+
     # Runs a game stage
-    def run_game_stage(self):
+    def run_game_stage(self) -> int:
         check_count = 0
 
-        print(self)
-
         while check_count != self.players.get_number_of_active_players():
-            for i in range(len(self.players)):
-                turn = (self.board.dealer + i + 1) % len(self.players)
+            game_over, value = self.game_stage_next(check_count)
+            if game_over:
+                return value
+            check_count = value
 
-                if self.players.has_folded(turn):
-                    continue
+    def game_stage_next(self, check_count: int) -> (bool, int):
+        """
+        Returns
+        -------
+        bool: True if the round is over, False otherwise
+        int: The winner of the round, if the round is over, else the check count
+        """
+        for i in range(len(self.players)):
+            turn = (self.board.dealer + i + 1) % len(self.players)
 
-                if self.players.get_number_of_active_players() == 1:
-                    return turn
+            if self.players.has_folded(turn):
+                continue
 
-                print(" ")
-                print(f"--- Player {turn}'s turn ---")
-                print(self.players.players[turn])
+            if self.players.get_number_of_active_players() == 1:
+                return True, turn
 
-                if self.game_stage == GameStage.PreFlop:
-                    x = self.preflop_bets(turn)
-                    if x is not None:
-                        check_count += x
-                        continue
+            print(" ")
+            print(f"--- Player {turn}'s turn ---")
+            print(self.players.players[turn])
 
-                action, amount = self.get_action()
+            if self.game_stage == GameStage.PreFlop:
+                check_count += self.preflop_bets(turn)
 
-                if action == Action.Fold:
-                    self.players.fold(turn)
-                    continue
-                elif action == Action.CallOrCheck:
-                    player_bet = self.players.get_bet(turn)
-                    bet = self.board.highest_bet - player_bet
-                    self.make_bet(turn, bet)
-                    if bet == 0:
-                        print("Checked")
-                    else:
-                        print(f"Called {bet}")
-                    check_count += 1
-                elif action == Action.Raise:
-                    player_bet = self.players.get_bet(turn)
-                    raise_amount = self.board.highest_bet - player_bet + amount
-                    self.make_bet(turn, raise_amount)
-                    check_count = 1
+            action, amount = self.get_action()
+
+            if action == Action.Fold:
+                self.players.fold(turn)
+                continue
+            elif action == Action.CallOrCheck:
+                player_bet = self.players.get_bet(turn)
+                bet = self.board.highest_bet - player_bet
+                if bet == 0:
+                    print("Checked")
+                    self.make_bet(turn, Action.Check, 0)
                 else:
-                    raise ValueError("Invalid action")
+                    print(f"Called {bet}")
+                    self.make_bet(turn, Action.Call, bet)
+                check_count += 1
+            elif action == Action.Raise:
+                player_bet = self.players.get_bet(turn)
+                raise_amount = self.board.highest_bet - player_bet + amount
+                self.make_bet(turn, Action.Raise, raise_amount)
+                check_count = 1
+            else:
+                raise ValueError("Invalid action")
 
-                if check_count == self.players.get_number_of_active_players():
-                    break
+            if check_count == self.players.get_number_of_active_players():
+                break
+        return False, check_count
 
-    # Handles the small and big blind
-    # Handles automatic betting for the small and big blind
-    # Assuming they can not fold.
-    # Returns 1 if the player is the big blind, 0 otherwise
-    def preflop_bets(self, turn: int):
+        # Handles the small and big blind
+        # Handles automatic betting for the small and big blind
+        # Assuming they can not fold.
+        # Returns 1 if the player is the big blind, 0 otherwise
+
+    def preflop_bets(self, turn: int) -> int:
         player_bet: int = self.players.get_bet(turn)
         small_blind = (self.board.dealer + 1) % len(self.players)
         big_blind = (self.board.dealer + 2) % len(self.players)
@@ -256,16 +220,24 @@ class GameManager:
 
         if turn == small_blind and self.board.highest_bet == 0:
             # Small blind
-            print(f"Player {turn} is the small blind and must bet {self.buy_in / 2}")
-            self.make_bet(turn, self.buy_in / 2)
+            print(
+                f"Player {turn} is the small blind and must bet {self.buy_in / 2}")
+            self.make_bet(turn, Action.Raise, self.buy_in / 2)
             return 0
         elif turn == big_blind and self.board.highest_bet == self.buy_in / 2:
             # Big blind
             print(f"Player {turn} is the big blind and must bet {self.buy_in}")
-            self.make_bet(turn, self.buy_in)
+            self.make_bet(turn, Action.Raise, self.buy_in)
             return 1
         else:
-            return None
+            return 0
+
+    def get_new_dealer(self, dealer: int):
+        dealer = (dealer + 1) % len(self.players)
+
+        if self.players.is_bust(dealer):
+            self.rotate_dealer()
+        return dealer
 
     def __str__(self):
         result = (
---
+++ project2/src/game_state/player_state.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+from typing import Tuple
+from src.poker_oracle.deck import Card
+from src.game_manager.game_action import Action
+
+
+@dataclass
+class PublicPlayerState:
+    def __init__(self,
+                 chips: int,
+                 folded: bool = False,
+                 bust: bool = False,
+                 bet: int = 0):
+        self.chips: int = chips
+        self.folded: bool = folded
+        self.bust: bool = False
+        self.round_bet: int = bet  # Betting for that round
+        self.betting_history = list()
+        self.action_history = list()
+
+
+class PrivatePlayerState(PublicPlayerState):
+    def __init__(self):
+        super().__init__(chips=100)
+
+    def _bet(self, amount) -> bool:
+        if self.chips < amount:
+            return False
+        self.round_bet += amount
+        self.chips -= amount
+        return True
+
+    def action(self, action: Action, amount: int = 0) -> bool:
+        """Returns a boolean indicating whether the action was successful"""
+        if self._bet(amount) is False:
+            # TODO: Do not need to fold. Can go all-in or check if possible
+            print("Not enough money. Folded.")
+            self.action_history.append(Action.Fold)
+            self.betting_history.append(0)
+            return False
+        self.action_history.append(action)
+        self.betting_history.append(0)
+        if action == Action.Fold:
+            self.folded = True
+        return True
+
+    def to_public(self) -> PublicPlayerState:
+        return PublicPlayerState(
+            chips=self.chips,
+            folded=self.folded,
+            bet=self.bet,
+        )
+
+    def reset_round(self, cards: Tuple[Card, Card]):
+        # Enforce type
+        if not isinstance(cards, tuple):
+            raise TypeError("cards must be a tuple")
+        if len(cards) != 2:
+            raise ValueError("cards must be a tuple of length 2")
+        if self.bust is False:
+            self.cards = cards
+        self.folded = False
+        self.bet = 0
+        self.betting_history = list()
+        self.action_history = list()
+
+    def __str__(self):
+        s = ""
+        if self.folded:
+            s = "Folded "
+        s += f"Cards: {self.cards[0]}, {self.cards[1]} Chips: {self.chips}"
+        return s

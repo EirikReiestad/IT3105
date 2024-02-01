64df2cb9248aa6179ab0b68b0e33273e32bffbd4
EirikReiestad <eirik@reiestad.no>
2024-02-01 16:09:01 +0100

    fix test

--- project2/tests/game_manager/test_manager.py
+++ project2/tests/game_manager/test_manager.py
@@ -1,11 +1,14 @@
 import unittest
 from src.game_manager.manager import GameManager
+from src.game_state.deck import Deck
 
 
 class TestGameManager(unittest.TestCase):
     def test_new(self):
         num_players = 2
-        game_manager = GameManager(num_players)
+        deck = Deck()
+        deck.reset_stack()
+        game_manager = GameManager(num_players, deck)
         self.assertEqual(len(game_manager.players), num_players)
 
 

854badf3b113c759837b3edc81888ae647f3a7a8
EirikReiestad <eirik@reiestad.no>
2024-02-11 19:02:42 +0100

    fixed test

--- project2/tests/game_manager/test_manager.py
+++ project2/tests/game_manager/test_manager.py
@@ -6,10 +6,11 @@ from src.poker_oracle.deck import Deck
 class TestGameManager(unittest.TestCase):
     def test_new(self):
         num_players = 2
+        num_ai = 1
         deck = Deck()
         deck.reset_stack()
-        game_manager = GameManager(num_players, deck)
-        self.assertEqual(len(game_manager.players), num_players)
+        game_manager = GameManager(num_players, deck, num_ai)
+        self.assertEqual(len(game_manager.players), num_players + num_ai)
 
 
 if __name__ == "__main__":
